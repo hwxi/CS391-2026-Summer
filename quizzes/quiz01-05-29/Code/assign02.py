a6643b58694aca2e08bd06aceee2d7ad5249fc9d
Hongwei <gmhwxi@gmail.com>
2026-05-29 10:18:52 -0400

    Updating: very very minorly

---
+++ quizzes/quiz01-05-29/Code/assign02.py
@@ -0,0 +1,17 @@
+############################################################
+#
+# Quiz01 of Friday, the 29th of May, 2026
+#
+# Please copy this file to the MySolution directory,
+# where all of your submitted files should be stored.
+#
+############################################################
+#
+# There are three tasks in this quiz.
+#
+# For EACH of your solution, please give a short and clean
+# description of it:
+# 1. What is your idea?
+# 2. Does your code work? Have you tested it?
+#
+############################################################
