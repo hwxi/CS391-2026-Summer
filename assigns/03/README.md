afc7449514bffe4d1fab14a829621d009459da5b
Hongwei <gmhwxi@gmail.com>
2026-06-02 20:23:56 -0400

    Updating: very very minorly

---
+++ assigns/03/README.md
@@ -0,0 +1,9 @@
+# Assignment 3 for CS391X1, Summer I, 2026
+
+## Total points: 100
+
+## Description of the task
+
+Please transate into LAMBDA the following problem (for solving the 8-queen puzzle):
+
+https://ats-lang.github.io/FROZEN000/DOCUMENT/INT2PROGINATS/HTML/INT2PROGINATS-BOOK-onechunk.html#example-the-eight-queens-puzzle
--- lectures/lecture-06-02/lambda2.py
+++ lectures/lecture-06-02/lambda2.py
@@ -1,12 +1,33 @@
 ############################################################
+# HX-2026-06-02:
+# This is the LAMBDA language so far:
+#
 # datatype term =
+#######
+# Primitive data
 # | TMint of sint
 # | TMbtf of bool
+#######
+# Pure part of LC
 # | TMvar of strn(*name*) // name
 # | TMlam of (strn, term) // abstraction
 # | TMapp of (term, term) // application
+#######
+# Primitive operations
 # | TMop2 of (strn(*name*), term(*arg1*), term(*arg2))
+#######
+# For if-then-else experssions
 # | TMifb of (term(*cond*), term(*then*), term(*else*))
+#######
+# For recursion via fixed-point
+# | TMfix of (strn(*f*), strn(*x*), term) // fixed-point
+#######
+# For pairs
+# | TMprj1 of (term) // 1st projection
+# | TMprj2 of (term) // 2nd projection
+# | TMpair of (term(*1st*), term(*snd*))
+#######
+#
 ############################################################
 #
 # HX-2026-05-20:
@@ -52,6 +73,16 @@ class term_lam(term):
     def __str__(self):
         return ("TMlam(" + self.arg1 + "," + str(self.arg2) + ")")
 # end-of-class(term_lam(term))
+
+class term_fix(term):
+    def __init__(self, arg1, arg2, arg3):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+        self.ctag = "TMfix"
+    def __str__(self):
+        return ("TMfix(" + self.arg1 + "," + self.arg2 + "," + str(self.arg3) + ")")
+# end-of-class(term_fix(term))
 
 class term_app(term):
     def __init__(self, arg1, arg2):
@@ -154,6 +185,8 @@ def term_size(tm0):
         return 1
     if (ctag == "TMlam"):
         return 1 + term_size(tm0.arg2)
+    if (ctag == "TMfix"):
+        return 1 + term_size(tm0.arg3)
     if (ctag == "TMapp"):
         return 1 + term_size(tm0.arg1) + term_size(tm0.arg2)
     if (ctag == "TMop2"):
@@ -199,6 +232,16 @@ def term_subst0(tm0, tx0, sub):
             return tm0 # no free 'tx1' in 'tm0'!!!
         else:
             return term_lam(tx1, subst0(tm1))
+    if (ctag == "TMfix"):
+        tf1 = tm0.arg1 # fixpnt-fun
+        tx1 = tm0.arg2 # fixpnt-arg
+        tm1 = tm0.arg3 # fixpnt-body
+        if (tx0 == tf1):
+            return tm0 # no free 'tx0' in 'tm0'!!!
+        if (tx0 == tx1):
+            return tm0 # no free 'tx0' in 'tm0'!!!
+        else:
+            return term_fix(tf1, tx1, subst0(tm1))
     if (ctag == "TMapp"):
         tm1 = tm0.arg1
         tm2 = tm0.arg2
@@ -239,6 +282,8 @@ def term_interp(tm0):
         return tm0
     if (ctag == "TMlam"):
         return tm0
+    if (ctag == "TMfix"):
+        return tm0
     if (ctag == "TMapp"):
         # tm0 = TMapp(tm1, tm2)
         tm1 = tm0.arg1
@@ -251,8 +296,17 @@ def term_interp(tm0):
             # tm0 = TMapp(TMlam(tm1_x, tm1_t), tm2)
             tm1_x = tm1.arg1
             tm1_t = tm1.arg2
-            tm0 = term_subst0(tm1_t, tm1_x, tm2)
-            return term_interp(tm0)
+            tm1_t = term_subst0(tm1_t, tm1_x, tm2)
+            return term_interp(tm1_t)
+        if (ctg1 == "TMfix"):
+            # tm1 = TMfix(tm1_f, tm1_x, tm1_t)
+            # tm0 = TMapp(TMfix(tm1_f, tm1_x, tm1_t), tm2)
+            tm1_f = tm1.arg1
+            tm1_x = tm1.arg2
+            tm1_t = tm1.arg3
+            tm1_t = term_subst0(tm1_t, tm1_f, tm1)
+            tm1_t = term_subst0(tm1_t, tm1_x, tm2)
+            return term_interp(tm1_t)
         # print("TypeError: non-function!")
         raise TypeError(tm1) # HX: TypeError: non-function
     if (ctag == "TMop2"):
@@ -394,4 +448,253 @@ _ = print("term_fibo(5) = ", term_interp(term_app(term_fibo, term_int(5))))
 _ = print("term_fibo(10) = ", term_interp(term_app(term_fibo, term_int(10))))
 
 ############################################################
+
+term_fact_fix = \
+term_fix("f", "n",
+  term_ifb(term_op2("<=", tvar_n, tint_0), tint_1,
+    term_op2("*", tvar_n, term_app(tvar_f, term_op2("-", tvar_n, tint_1)))))
+_ = print("term_fact_fix = ", term_fact_fix)
+
+_ = print("term_fact_fix(3) = ", term_interp(term_app(term_fact_fix, term_int(3))))
+_ = print("term_fact_fix(10) = ", term_interp(term_app(term_fact_fix, term_int(10))))
+
 ############################################################
+
+term_fibo_fix = \
+term_fix("f", "n",
+  term_ifb(term_op2("<=", tvar_n, tint_1), tvar_n,
+    term_op2("+", term_app(tvar_f, term_op2("-", tvar_n, tint_2)), term_app(tvar_f, term_op2("-", tvar_n, tint_1)))))
+_ = print("term_fibo_fix = ", term_fibo_fix)
+
+_ = print("term_fibo_fix(5) = ", term_interp(term_app(term_fibo_fix, term_int(5))))
+_ = print("term_fibo_fix(10) = ", term_interp(term_app(term_fibo_fix, term_int(10))))
+
+############################################################
+############################################################
+#
+# datatype value =
+# | VALint of sint
+# | VALbtf of bool
+# | VALlam of (term(*TMlam*), vlenv)
+# | VALfix of (term(*TMfix*), vlenv)
+# | VALpair of (value(*1st*), value(*2nd*))
+#
+class value:
+    ctag = ""
+    def __str__(self):
+        return ("value(" + self.ctag + ")")
+# end-of-class(value)
+
+class value_int(value):
+    def __init__(self, arg1):
+        self.arg1 = arg1
+        self.ctag = "VALint"
+    def __str__(self):
+        return ("VALint(" + str(self.arg1) + ")")
+# end-of-class(value_int(value))
+
+class value_btf(value):
+    def __init__(self, arg1):
+        self.arg1 = arg1
+        self.ctag = "VALbtf"
+    def __str__(self):
+        return ("VALbtf(" + str(self.arg1) + ")")
+# end-of-class(value_btf(value))
+
+class value_lam(value):
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.ctag = "VALlam"
+    def __str__(self):
+        return ("VALlam(" + self.arg1 + "," + "..." + ")")
+# end-of-class(value_lam(term)
+
+class value_fix(value):
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.ctag = "VALfix"
+    def __str__(self):
+        return ("VALfix(" + self.arg1 + "," + "..." + ")")
+# end-of-class(value_fix(term))
+
+############################################################
+#
+# datatyp vlenv =
+# | ENVnil of ()
+# | ENVcons of (strn(*x*), value, vlenv)
+#
+class vlenv:
+    ctag = ""
+    def __str__(self):
+        return ("vlenv(" + self.ctag + ")")
+# end-of-class(vlenv)
+
+class vlenv_nil(vlenv):
+    def __init__(self):
+        self.ctag = "ENVnil"
+    def __str__(self):
+        return ("ENVnil(" + ")")
+# end-of-class(vlenv_nil(vlenv))
+
+class vlenv_cons(vlenv):
+    def __init__(self, arg1, arg2, arg3):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+        self.ctag = "ENVcons"
+    def __str__(self):
+        return ("ENVcons(" + self.arg1 + "," + str(self.arg2) + "," + str(self.arg3) + ")")
+# end-of-class(vlenv_cons(vlenv))
+
+############################################################
+############################################################
+
+def vlenv_search(env0, tx):
+    ctag = env0.ctag
+    if ctag == "ENVnil":
+      return None
+    if ctag == "ENVcons":
+        if tx == env0.arg1:
+            return env0.arg2
+        else:
+            return vlenv_search(env0.arg3, tx)
+    raise TypeError(env0) # HX-2026-06-02: should be deadcode!    
+
+############################################################
+
+def term_evaluate(tm0):
+    env0 = vlenv_nil()
+    return term_enveval(tm0, env0)
+
+def term_enveval(tm0, env0):
+    # print("term_enveval: tm0 = ", tm0)
+    ctag = tm0.ctag
+    if ctag == "TMint":
+        return value_int(tm0.arg1)
+    if ctag == "TMbtf":
+        return value_btf(tm0.arg1)
+    if (ctag == "TMvar"):
+        return vlenv_search(env0, tm0.arg1)
+    if (ctag == "TMlam"):
+        return value_lam(tm0, env0)
+    if (ctag == "TMfix"):
+        return value_fix(tm0, env0)
+    if (ctag == "TMapp"):
+        # tm0 = TMapp(tm1, tm2)
+        tm1 = tm0.arg1
+        tm2 = tm0.arg2
+        vl1 = term_enveval(tm1, env0)
+        vl2 = term_enveval(tm2, env0)
+        ctg1 = vl1.ctag
+        # print("TMapp: ctg1 = ", ctg1)
+        if (ctg1 == "VALlam"):
+            vl1_tm1 = vl1.arg1
+            vl1_env0 = vl1.arg2
+            vl1_tm1_x = vl1_tm1.arg1
+            vl1_tm1_t = vl1_tm1.arg2
+            vl1_env1 = \
+                vlenv_cons(vl1_tm1_x, vl2, vl1_env0)
+            return term_enveval(vl1_tm1_t, vl1_env1)
+        if (ctg1 == "VALfix"):
+            vl1_tm1 = vl1.arg1
+            vl1_env0 = vl1.arg2
+            vl1_tm1_f = vl1_tm1.arg1
+            vl1_tm1_x = vl1_tm1.arg2
+            vl1_tm1_t = vl1_tm1.arg3
+            vl1_env1 = \
+                vlenv_cons(vl1_tm1_f, vl1, vl1_env0)
+            vl1_env2 = \
+                vlenv_cons(vl1_tm1_x, vl2, vl1_env1)
+            return term_enveval(vl1_tm1_t, vl1_env2)
+        # print("TypeError: non-function!")
+        raise TypeError(tm1) # HX: TypeError: non-function
+    if (ctag == "TMop2"):
+        # tm0 = TMop2(tm1, tm2, tm3)
+        nm1 = tm0.arg1
+        vl2 = term_enveval(tm0.arg2, env0)
+        vl3 = term_enveval(tm0.arg3, env0)
+        if (nm1 == "+"):
+            assert vl2.ctag == "VALint"
+            assert vl3.ctag == "VALint"
+            return value_int(vl2.arg1 + vl3.arg1)
+        if (nm1 == "-"):
+            assert vl2.ctag == "VALint"
+            assert vl3.ctag == "VALint"
+            return value_int(vl2.arg1 - vl3.arg1)
+        if (nm1 == "*"):
+            assert vl2.ctag == "VALint"
+            assert vl3.ctag == "VALint"
+            return value_int(vl2.arg1 * vl3.arg1)
+        if (nm1 == "%"):
+            assert vl2.ctag == "VALint"
+            assert vl3.ctag == "VALint"
+            return value_int(vl2.arg1 % vl3.arg1)
+        if (nm1 == "/"):
+            assert vl2.ctag == "VALint"
+            assert vl3.ctag == "VALint"
+            return value_int(vl2.arg1 // vl3.arg1)
+        if (nm1 == ">"):
+            assert vl2.ctag == "VALint"
+            assert vl3.ctag == "VALint"
+            return value_btf(vl2.arg1 > vl3.arg1)
+        if (nm1 == "<"):
+            assert vl2.ctag == "VALint"
+            assert vl3.ctag == "VALint"
+            return value_btf(vl2.arg1 < vl3.arg1)
+        if (nm1 == ">="):
+            assert vl2.ctag == "VALint"
+            assert vl3.ctag == "VALint"
+            return value_btf(vl2.arg1 >= vl3.arg1)
+        if (nm1 == "<="):
+            assert vl2.ctag == "VALint"
+            assert vl3.ctag == "VALint"
+            return value_btf(vl2.arg1 <= vl3.arg1)
+        if (nm1 == "=="):
+            assert vl2.ctag == "VALint"
+            assert vl3.ctag == "VALint"
+            return value_btf(vl2.arg1 == vl3.arg1)
+        if (nm1 == "!="):
+            assert vl2.ctag == "VALint"
+            assert vl3.ctag == "VALint"
+            return value_btf(vl2.arg1 != vl3.arg1)
+        raise TypeError(nm1) # HX: TypeError: non-supported
+    if (ctag == "TMifb"):
+        # tm0 = TMifb(tm1, tm2, tm3)
+        tm1 = tm0.arg1
+        vl1 = term_enveval(tm1, env0)
+        ctg1 = vl1.ctag
+        # print("TMifb: ctg1 = ", ctg1)
+        if (ctg1 == "VALbtf"):
+            # vl1 = VALbtf(btf)
+            if (vl1.arg1):
+                return term_enveval(tm0.arg2, env0)
+            else:
+                return term_enveval(tm0.arg3, env0)
+        # print("TypeError: non-boolean-cond!")
+        raise TypeError(tm1) # HX: TypeError: non-boolean        
+    raise TypeError(tm1) # HX: TypeError: term_enveval(...)
+############################################################
+
+_ = print("evaluate(tint_2) = ", term_evaluate(tint_2))
+_ = print("evaluate(tbtf_t) = ", term_evaluate(tbtf_t))
+_ = print("evaluate(tbtf_f) = ", term_evaluate(tbtf_f))
+_ = print("evaluate(term_I(tint_2)) = ", term_evaluate(term_app(term_I, tint_2)))
+_ = print("evaluate(term_I(tbtf_t)) = ", term_evaluate(term_app(term_I, tbtf_t)))
+_ = print("evaluate(term_dbl(tint_2)) = ", term_evaluate(term_app(term_dbl, tint_2)))
+_ = print("evaluate(term_sqr(tint_2)) = ", term_evaluate(term_app(term_sqr, tint_2)))
+
+############################################################
+
+term_fact_fix = \
+term_fix("f", "n",
+  term_ifb(term_op2("<=", tvar_n, tint_0), tint_1,
+    term_op2("*", tvar_n, term_app(tvar_f, term_op2("-", tvar_n, tint_1)))))
+_ = print("term_fact_fix = ", term_fact_fix)
+
+_ = print("evaluate(term_fact_fix(3)) = ", term_evaluate(term_app(term_fact_fix, term_int(3))))
+_ = print("evaluate(term_fact_fix(10)) = ", term_evaluate(term_app(term_fact_fix, term_int(10))))
+
+############################################################
+
