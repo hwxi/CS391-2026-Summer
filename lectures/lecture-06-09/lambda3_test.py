ffcb6abe494c94f7176d4d682737dc5644a214db
Hongwei <gmhwxi@gmail.com>
2026-06-11 11:56:47 -0400

    Updating: very very minorly

--- lectures/lecture-06-09/lambda3_test.py
+++ lectures/lecture-06-09/lambda3_test.py
@@ -34,11 +34,14 @@ DE_f = DEvar("f")
 DE_n = DEvar("n")
 DE_x = DEvar("x")
 ##################################################################
+
 # let x = 2+3 in x*x
 dexp_let1 = DElet("x", DEop2("+", DE_2, DE_3), DEop2("*", DE_x, DE_x))
 _ = print("dexp_let1 = ", dexp_let1)
 _ = print("dexp_let1 = ", dexp_eval000(dexp_let1))
+
 ##################################################################
+
 dexp_fact_fix = \
 DEfix("f", "n",
   DEif0(DEop2("<=", DE_n, DE_0), DE_1,
@@ -47,6 +50,9 @@ _ = print("dexp_fact_fix = ", dexp_fact_fix)
 _ = print("dexp_fact_fix(3) = ", dexp_eval000(DEapp(dexp_fact_fix, DEint(3))))
 _ = print("dexp_fact_fix(5) = ", dexp_eval000(DEapp(dexp_fact_fix, DEint(5))))
 _ = print("dexp_fact_fix(10) = ", dexp_eval000(DEapp(dexp_fact_fix, DEint(10))))
+
+##################################################################
 ##################################################################
 # end of [CS391-2026-Summer/lectures/lecture-06-09/lambda3_test.py]
+##################################################################
 ##################################################################
---
+++ lectures/lecture-06-15/lambda5.py
@@ -0,0 +1,134 @@
+##################################################################
+#
+# HX: let's use dataclasses
+# HX: let's do type-checking
+# Tue Jun 15 08:08:43 AM EDT 2026
+#
+##################################################################
+type nint = int
+type sint = int
+type strn = str
+##################################################################
+from abc import ABC
+from enum import Enum
+from dataclasses import dataclass
+##################################################################
+type styp = ST000
+##################################################################
+type dvar = strn
+type dexp = DE000
+##################################################################
+@dataclass
+class ST000(ABC):
+    pass
+class STbas(ST000):
+    arg1: strn
+    pass
+class STfun(ST000):
+    arg1: styp
+    arg2: styp
+    pass
+class STtup(ST000):
+    arg1: styp
+    arg2: styp
+    pass
+class STlist(ST000):
+    arg1: styp
+    pass
+class STstrm(ST000):
+    arg1: styp
+    pass
+class STstcn(ST000):
+    arg1: styp
+    pass
+##################################################################
+@dataclass
+class DE000(ABC):
+    pass
+@dataclass
+class DEint(DE000):
+    arg1: sint
+    pass
+@dataclass
+class DEbtf(DE000):
+    arg1: bool
+    pass
+@dataclass
+class DEstr(DE000):
+    arg1: strn
+    pass
+@dataclass
+class DEvar(DE000):
+    arg1: dvar
+    pass
+@dataclass
+class DElam(DE000):
+    arg1: dvar
+    arg2: dexp
+    pass
+@dataclass
+class DEapp(DE000):
+    arg1: dexp
+    arg2: dexp
+    pass
+@dataclass
+class DEop1(DE000):
+    arg1: strn
+    arg2: dexp
+    pass
+@dataclass
+class DEop2(DE000):
+    arg1: strn
+    arg2: dexp
+    arg3: dexp
+    pass
+@dataclass
+class DEif0(DE000):
+    arg1: dexp
+    arg2: dexp
+    arg3: dexp
+    pass
+@dataclass
+class DEfst(DE000):
+    arg1: dexp
+    pass
+@dataclass
+class DEsnd(DE000):
+    arg1: dexp
+    pass
+@dataclass
+class DEtup(DE000):
+    arg1: dexp
+    arg2: dexp
+    pass
+@dataclass
+class DEfix(DE000):
+    arg1: dvar
+    arg2: dvar
+    arg3: dexp
+    pass
+@dataclass
+class DElet(DE000):
+    arg1: dvar
+    arg2: dexp
+    arg3: dexp
+    pass
+@dataclass
+class DElam1(DE000):
+    arg1: dvar
+    arg2: styp
+    arg3: dexp
+    pass
+@dataclass
+class DEfix1(DE000):
+    arg1: dvar
+    arg2: dvar
+    arg3: styp
+    arg4: dexp
+    arg5: styp
+    pass
+##################################################################
+##################################################################
+# end of [CS391-2026-Summer/lectures/lecture-06-09/lambda3.py]
+##################################################################
+##################################################################
