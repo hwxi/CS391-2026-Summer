dc65f2d58dabe6bea1c955b682badeb087a2623f
Hongwei <gmhwxi@gmail.com>
2026-06-16 19:16:20 -0400

    Updating: very very minorly

--- lectures/lecture-06-16/lambda5.py
+++ lectures/lecture-06-16/lambda5.py
@@ -21,26 +21,32 @@ type dexp = DE000
 @dataclass
 class ST000(ABC):
     pass
+##################################################################
+@dataclass
 class STbas(ST000):
     arg1: strn
     pass
-class STxyz(ST000):
-    arg1: styp
-    pass
+##################################################################
+@dataclass
 class STfun(ST000):
     arg1: styp
     arg2: styp
     pass
+@dataclass
 class STtup(ST000):
     arg1: styp
     arg2: styp
     pass
+##################################################################
+@dataclass
 class STlist(ST000):
     arg1: styp
     pass
+@dataclass
 class STstrm(ST000):
     arg1: styp
     pass
+@dataclass
 class STstcn(ST000):
     arg1: styp
     pass
@@ -127,9 +133,9 @@ class DElam1(DE000):
 class DEfix1(DE000):
     arg1: dvar
     arg2: dvar
-    arg3: styp
-    arg4: dexp
-    arg5: styp
+    arg3: styp # arg
+    arg4: styp # res
+    arg5: dexp
     pass
 @dataclass
 class DEanno(DE000):
@@ -157,14 +163,147 @@ type stypopt = styp|None
 
 ##################################################################
 
+STint = STbas("sint")
+STbtf = STbas("bool")
+STstr = STbas("strn")
+
+##################################################################
+
 def \
-dexp_oftp000(dex: dexp) -> stypopt:
+xtctx_search\
+(xts: xtctx, dx0: dvar) -> styp:
+    while True:
+        if isinstance(xts, SCTXnil):
+            return ST000()
+        if isinstance(xts, SCTXcons):
+            if dx0 == xts.arg1:
+                return xts.arg2
+            else:
+                xts = xts.arg3; continue
+        raise TypeError(xts) # HX-2026-06-02: should be deadcode!
+
+##################################################################
+
+# def dexp_eval000(dex: dexp):
+#    return dexp_evalenv(dex, ENVnil())
+def \
+dexp_oftp000(dex: dexp) -> styp:
     return dexp_oftpctx(dex, SCTXnil())
 
+# def \
+# dexp_evalenv(dex: dexp, env0: xvenv) -> dval:
 def \
-dexp_oftpctx(dex: dexp, ctx: xtctx) -> stypopt:
-    pass
+dexp_oftpctx(dex: dexp, ctx0: xtctx) -> styp:
+    if isinstance(dex, DEint):
+        return STint
+    if isinstance(dex, DEbtf):
+        return STbtf
+    if isinstance(dex, DEstr):
+        return STstr
+    if isinstance(dex, DEvar):
+        return \
+            xtctx_search(ctx0, dex.arg1)
+    if isinstance(dex, DEapp):
+        # DEapp(e1, e2)
+        dex1 = dex.arg1
+        dex2 = dex.arg2
+        tex1 = dexp_oftpctx(dex1, ctx0)
+        tex2 = dexp_oftpctx(dex2, ctx0)
+        assert isinstance(tex1, STfun)
+        assert (tex1.arg1 == tex2)
+        return tex1.arg2
+    if isinstance(dex, DEop2):
+        opnm = dex.arg1
+        dex1 = dex.arg2
+        dex2 = dex.arg3
+        tex1 = dexp_oftpctx(dex1, ctx0)
+        tex2 = dexp_oftpctx(dex2, ctx0)
+        return dop2_oftp(opnm, tex1, tex2)
+    if isinstance(dex, DEif0):
+        cond = dex.arg1
+        tcnd = dexp_oftpctx(cond, ctx0)
+        assert tcnd == STbtf
+        tthn = dexp_oftpctx(dex.arg2, ctx0)
+        tels = dexp_oftpctx(dex.arg3, ctx0)
+        assert tthn == tels
+        return tthn
+    if isinstance(dex, DElam1):
+        # DElam1(x1, T1, e2)
+        xarg = dex.arg1
+        targ = dex.arg2
+        body = dex.arg3
+        ctx1 = SCTXcons(xarg, targ, ctx0)
+        tres = dexp_oftpctx(body, ctx1)
+        return STfun(targ, tres)
+    if isinstance(dex, DEfix1):
+        # DEfix1(f0, x1, T1, T2, e2)
+        farg = dex.arg1
+        xarg = dex.arg2
+        targ = dex.arg3
+        tres = dex.arg4
+        tfun = STfun(targ, tres)
+        body = dex.arg5
+        ctx1 = SCTXcons(farg, tfun, ctx0)
+        ctx2 = SCTXcons(xarg, targ, ctx1)
+        trs2 = dexp_oftpctx(body, ctx2)
+        assert tres == trs2
+        return tfun
+    raise TypeError(dex) # HX-2026-06-15: dexp_oftpctx(...)
 
+##################################################################
+
+def dop2_oftp\
+(opnm: strn, tex1: styp, tex2: styp) -> styp:
+    # print("dop2_oftp: opnm = ", opnm)
+    # print("dop2_oftp: tex1 = ", tex1)
+    # print("dop2_oftp: tex2 = ", tex2)
+    if (opnm == "+"):
+        assert (tex1 == STint)
+        assert (tex2 == STint)
+        return STint
+    if (opnm == "-"):
+        assert (tex1 == STint)
+        assert (tex2 == STint)
+        return STint
+    if (opnm == "*"):
+        assert (tex1 == STint)
+        assert (tex2 == STint)
+        return STint
+    if (opnm == "%"):
+        assert (tex1 == STint)
+        assert (tex2 == STint)
+        return STint
+    if (opnm == "/"):
+        assert (tex1 == STint)
+        assert (tex2 == STint)
+        return STint
+    if (opnm == "<"):
+        assert (tex1 == STint)
+        assert (tex2 == STint)
+        return STbtf
+    if (opnm == ">"):
+        assert (tex1 == STint)
+        assert (tex2 == STint)
+        return STbtf
+    if (opnm == "<="):
+        assert (tex1 == STint)
+        assert (tex2 == STint)
+        return STbtf
+    if (opnm == ">="):
+        assert (tex1 == STint)
+        assert (tex2 == STint)
+        return STbtf
+    if (opnm == "=="):
+        assert (tex1 == STint)
+        assert (tex2 == STint)
+        return STbtf
+    if (opnm == "!="):
+        assert (tex1 == STint)
+        assert (tex2 == STint)
+        return STbtf
+    raise TypeError(opnm) # HX-2026-06-09: dop2_oftp(...)
+
+##################################################################
 ##################################################################
 # end of [CS391-2026-Summer/lectures/lecture-06-09/lambda5.py]
 ##################################################################
--- lectures/lecture-06-16/lambda5_test.py
+++ lectures/lecture-06-16/lambda5_test.py
@@ -11,6 +11,16 @@ from lambda5 import *
 ##################################################################
 sys.setrecursionlimit(10000)
 ##################################################################
+STint = STbas("sint")
+STbtf = STbas("bool")
+STstr = STbas("strn")
+STfun_i_i = STfun(STint, STint)
+# STfun_i_i_i: Haskell Curry style
+STfun_i_i_i = STfun(STint, STfun(STint, STint))
+STfun_ixi_i = STfun(STtup(STint, STint), STint)
+_ = print("STfun_i_i_i = " + repr(STfun_i_i_i))
+_ = print("STfun_ixi_i = " + repr(STfun_ixi_i))
+##################################################################
 DE_0 = DEint(0)
 DE_1 = DEint(1)
 DE_2 = DEint(2)
@@ -18,6 +28,24 @@ DE_3 = DEint(3)
 DE_f = DEvar("f")
 DE_n = DEvar("n")
 DE_x = DEvar("x")
+##################################################################
+
+dex01 = DElam1("x", STint, DE_0)
+dex02 = DElam1("x", STint, DE_x)
+dex03 = DEapp(dex01, DE_1)
+dex04 = DEapp(dex02, DEbtf(True))
+print("oftp(dex01) = ", dexp_oftp000(dex01))
+print("oftp(dex02) = ", dexp_oftp000(dex02))
+print("oftp(dex03) = ", dexp_oftp000(dex03))
+# print("oftp(dex04) = ", dexp_oftp000(dex04))
+
+##################################################################
+dexp_fact_fix1 = \
+DEfix1("f", "n", STint, STint,
+  DEif0(DEop2("<=", DE_n, DE_0), DE_1,
+    DEop2("*", DE_n, DEapp(DE_f, DEop2("-", DE_n, DE_1)))))
+_ = print("dexp_fact_fix1 = ", dexp_fact_fix1)
+_ = print("oftp(dexp_fact_fix1) = ", dexp_oftp000(dexp_fact_fix1))
 ##################################################################
 ##################################################################
 # end of [CS391-2026-Summer/lectures/lecture-06-15/lambda5_test.py]
